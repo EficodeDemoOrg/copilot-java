72b7f9ecf41d21db186668889eb12f671facd149
Rasmus Paltschik <rasmus.paltschik@eficode.com>
2025-05-19 10:40:43 +0300

    Add sikrit

--- src/main/java/com/weather/app/WeatherApp.java
+++ src/main/java/com/weather/app/WeatherApp.java
@@ -13,7 +13,7 @@ public class WeatherApp {
     
     private static final Logger LOGGER = Logger.getLogger(WeatherApp.class.getName());
 
-    // Secret for accessing Atlassian API! (Not really, it's deprecated)
+    // Secret for accessing Atlassian API!! (Not really, it's deprecated)
     private static final String SECRET_PAT = 
         "ATATT3xFfGF0wp8k76Z0Q2Wc2sP0NhHIlTALaCZR_CZxw8vuwsyt5Jijh-Zoem712l0jIAUjzn7hbdQ2" +
         "vOz3dUloyFR2oFtU26VjImYu0a5opr5AoCsuiIDKfiWgxwyu_oe-IMYURIQmea5x8CPBXMhkeD9rJbPZGOy-BbrnH74s9Dap_U=4900D7F8";
